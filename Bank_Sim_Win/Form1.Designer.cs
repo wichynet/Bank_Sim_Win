5a434d31cfbdd5f7915c19c24bbc2840af0ab440
Wichaya Nopakao <wichynet@gmail.com>
2019-06-16 16:31:42 +0700

    Deposite Tab

--- Bank_Sim_Win/Form1.Designer.cs
+++ Bank_Sim_Win/Form1.Designer.cs
@@ -30,15 +30,21 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtIbanNumber = new System.Windows.Forms.TextBox();
+            this.btnCreateNewAccount = new System.Windows.Forms.Button();
             this.txtFullName = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.btnCreateNewAccount = new System.Windows.Forms.Button();
+            this.txtCreateIbanNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.btnDeposite = new System.Windows.Forms.Button();
+            this.txtDepoIbanNumber = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDepoAmount = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -57,7 +63,7 @@
             this.tabPage1.Controls.Add(this.btnCreateNewAccount);
             this.tabPage1.Controls.Add(this.txtFullName);
             this.tabPage1.Controls.Add(this.label2);
-            this.tabPage1.Controls.Add(this.txtIbanNumber);
+            this.tabPage1.Controls.Add(this.txtCreateIbanNumber);
             this.tabPage1.Controls.Add(this.label1);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
@@ -67,41 +73,14 @@
             this.tabPage1.Text = "Create new account";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // btnCreateNewAccount
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(650, 307);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Deposite Money";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // tabPage3
-            // 
-            this.tabPage3.Location = new System.Drawing.Point(4, 22);
-            this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(650, 307);
-            this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "Transfer Money";
-            this.tabPage3.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 52);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "IBAN No.: ";
-            this.label1.Click += new System.EventHandler(this.Label1_Click);
-            // 
-            // txtIbanNumber
-            // 
-            this.txtIbanNumber.Location = new System.Drawing.Point(70, 49);
-            this.txtIbanNumber.Name = "txtIbanNumber";
-            this.txtIbanNumber.Size = new System.Drawing.Size(139, 20);
-            this.txtIbanNumber.TabIndex = 1;
+            this.btnCreateNewAccount.Location = new System.Drawing.Point(70, 76);
+            this.btnCreateNewAccount.Name = "btnCreateNewAccount";
+            this.btnCreateNewAccount.Size = new System.Drawing.Size(124, 23);
+            this.btnCreateNewAccount.TabIndex = 4;
+            this.btnCreateNewAccount.Text = "Create new account";
+            this.btnCreateNewAccount.UseVisualStyleBackColor = true;
             // 
             // txtFullName
             // 
@@ -121,14 +100,87 @@
             this.label2.Text = "Full Name:";
             this.label2.Click += new System.EventHandler(this.Label2_Click);
             // 
-            // btnCreateNewAccount
+            // txtCreateIbanNumber
             // 
-            this.btnCreateNewAccount.Location = new System.Drawing.Point(70, 76);
-            this.btnCreateNewAccount.Name = "btnCreateNewAccount";
-            this.btnCreateNewAccount.Size = new System.Drawing.Size(124, 23);
-            this.btnCreateNewAccount.TabIndex = 4;
-            this.btnCreateNewAccount.Text = "Create new account";
-            this.btnCreateNewAccount.UseVisualStyleBackColor = true;
+            this.txtCreateIbanNumber.Location = new System.Drawing.Point(70, 49);
+            this.txtCreateIbanNumber.Name = "txtCreateIbanNumber";
+            this.txtCreateIbanNumber.Size = new System.Drawing.Size(139, 20);
+            this.txtCreateIbanNumber.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IBAN No.: ";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.btnDeposite);
+            this.tabPage2.Controls.Add(this.txtDepoIbanNumber);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.txtDepoAmount);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(650, 307);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Deposite Money";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(650, 307);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Transfer Money";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // btnDeposite
+            // 
+            this.btnDeposite.Location = new System.Drawing.Point(100, 76);
+            this.btnDeposite.Name = "btnDeposite";
+            this.btnDeposite.Size = new System.Drawing.Size(124, 23);
+            this.btnDeposite.TabIndex = 9;
+            this.btnDeposite.Text = "Deposite";
+            this.btnDeposite.UseVisualStyleBackColor = true;
+            // 
+            // txtDepoIbanNumber
+            // 
+            this.txtDepoIbanNumber.Location = new System.Drawing.Point(100, 23);
+            this.txtDepoIbanNumber.Name = "txtDepoIbanNumber";
+            this.txtDepoIbanNumber.Size = new System.Drawing.Size(139, 20);
+            this.txtDepoIbanNumber.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "IBAN No.:";
+            // 
+            // txtDepoAmount
+            // 
+            this.txtDepoAmount.Location = new System.Drawing.Point(100, 49);
+            this.txtDepoAmount.Name = "txtDepoAmount";
+            this.txtDepoAmount.Size = new System.Drawing.Size(139, 20);
+            this.txtDepoAmount.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Deposite Amount";
             // 
             // Form1
             // 
@@ -141,6 +193,8 @@
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -152,10 +206,15 @@
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtIbanNumber;
+        private System.Windows.Forms.TextBox txtCreateIbanNumber;
         private System.Windows.Forms.TextBox txtFullName;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnCreateNewAccount;
+        private System.Windows.Forms.Button btnDeposite;
+        private System.Windows.Forms.TextBox txtDepoIbanNumber;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDepoAmount;
+        private System.Windows.Forms.Label label4;
     }
 }
 
